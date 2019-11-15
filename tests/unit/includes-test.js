844ee0bc4e601fc5c62740771f5f45a46884f75b
shibulijack <shibulijack@SHIBULIJACK-2.local>
2019-11-15 13:19:06 +0530

    Renamed contains to includes

--- tests/unit/includes-test.js
+++ tests/unit/includes-test.js
@@ -2,24 +2,24 @@ import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 import setupCustomAssertions from 'ember-qunit-custom-assertions/test-support';
 
-module('Assertion | contains', function(hooks) {
+module('Assertion | includes', function(hooks) {
   setupTest(hooks);
   setupCustomAssertions(hooks);
 
   test('can check arrays', function(assert) {
     let sampleArr = [1, 2, 3];
-    assert.contains(sampleArr, 2, 'Array contains element');
+    assert.includes(sampleArr, 2, 'Array includes element');
   });
 
   test('can check string', function(assert) {
     let sampleStr = "Developed by Freshworks";
-    assert.contains(sampleStr, 'Fresh', 'String contains sub string');
+    assert.includes(sampleStr, 'Fresh', 'String includes sub string');
   });
 
   test('can check objects', function(assert) {
     let sampleObj = {
       "foo": "bar"
     };
-    assert.contains(sampleObj, 'foo', 'Object contains key');
+    assert.includes(sampleObj, 'foo', 'Object includes key');
   });
 });
--- tests/unit/not-includes-test.js
+++ tests/unit/not-includes-test.js
@@ -2,24 +2,24 @@ import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 import setupCustomAssertions from 'ember-qunit-custom-assertions/test-support';
 
-module('Assertion | notContains', function(hooks) {
+module('Assertion | notIncludes', function(hooks) {
   setupTest(hooks);
   setupCustomAssertions(hooks);
 
   test('can check arrays', function(assert) {
     let sampleArr = [1, 2, 3];
-    assert.notContains(sampleArr, 4, 'Array does not contain element');
+    assert.notIncludes(sampleArr, 4, 'Array does not contain element');
   });
 
   test('can check string', function(assert) {
     let sampleStr = "Developed by Freshworks";
-    assert.notContains(sampleStr, 'Shibu', 'String does not have any matches');
+    assert.notIncludes(sampleStr, 'Shibu', 'String does not have any matches');
   });
 
   test('can check objects', function(assert) {
     let sampleObj = {
       "foo": "bar"
     };
-    assert.notContains(sampleObj, 'test', 'Object does not contain the specified key');
+    assert.notIncludes(sampleObj, 'test', 'Object does not contain the specified key');
   });
 });
