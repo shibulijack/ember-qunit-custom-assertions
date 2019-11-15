1aa3b6f903ff93d4749819133da495caa174081e
shibulijack <shibulijack@SHIBULIJACK-2.local>
2019-11-15 14:11:00 +0530

    Cleaned up code

--- test-support/assertions/includes.js
+++ test-support/assertions/includes.js
@@ -9,16 +9,13 @@ export default function(context, value, expected, message) {
   ) {
     result = value.includes(expected);
   } else if (typeof value === "object") {
-    if(typeof expected === "string") {
+    // Expected arg can be either `string` or `object`
+    if (typeof expected === "string") {
       result = value.hasOwnProperty(expected);
     } else {
-      let isAllKeysIncluded = true;
-      Object.keys(expected).forEach((key) => {
-        if(!value.hasOwnProperty(key) || value[key] != expected[key]) {
-          isAllKeysIncluded = false;
-        }
-      });
-      result = isAllKeysIncluded;
+      result = Object.keys(expected).every(
+        key => value.hasOwnProperty(key) && value[key] === expected[key]
+      );
     }
   }
 
--- tests/unit/includes-test.js
+++ tests/unit/includes-test.js
@@ -11,6 +11,11 @@ module('Assertion | includes', function(hooks) {
     assert.includes(sampleArr, 2, 'Array includes element');
   });
 
+  // test('can check complex arrays', function(assert) {
+  //   let sampleArr = [{x: 1}, {y: 2}, {z: 3}];
+  //   assert.includes(sampleArr, {x: 1}, 'Array includes element');
+  // });
+
   test('can check string', function(assert) {
     let sampleStr = "Developed by Freshworks";
     assert.includes(sampleStr, 'Fresh', 'String includes sub string');
@@ -24,12 +29,8 @@ module('Assertion | includes', function(hooks) {
   });
 
   test('can check complex objects', function(assert) {
-    let sampleObj = {
-      "foo": "bar"
-    };
-    let expectedObj = {
-      "foo": "bar"
-    }
-    assert.includes(sampleObj, expectedObj);
+    let sampleObj = {x: 1, y: 2};
+    let expectedObj = {x: 1};
+    assert.includes(sampleObj, expectedObj, 'Expected object is found in target object');
   });
 });
