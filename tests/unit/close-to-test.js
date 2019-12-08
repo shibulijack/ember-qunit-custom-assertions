a26227c213b581d36a6e78c45d4789e158bb6568
Vinodh Kumar <39614850+vinodh-kumar-1711@users.noreply.github.com>
2019-12-08 21:35:49 +0530

    Close to bug fix (#7)
    * closeTo bug fix
    
    * Updated package json
    
    * Typo fix

--- tests/unit/close-to-test.js
+++ tests/unit/close-to-test.js
@@ -6,5 +6,10 @@ module('Assertion | close-to', function(hooks) {
 
   test('check where the number as a delta of some amount', function(assert) {
     assert.closeTo(1.5, 1, 0.5);
+    assert.closeTo(1.7, 1, 1);
+    assert.closeTo(25, 28, 5);
+    assert.closeTo(25, 28, 3);
+    assert.closeTo(28, 25, 5);
+    assert.closeTo(28, 25, 3);
   });
 });
